3f84d6188b75ecdc30120e7a3f846446e33fa66d
Alexandra Pyvovarchuk <alisandra.pv@gmail.com>
2019-11-15 03:15:30 +0200

    com

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ class Root {
         this.json_winners_path = "./winners.json";
         this.user_table_body = document.getElementById('user_table');
         this.winners_output = document.getElementById('winners_output');
+        this.arr_winners_id = [];
+        this.btn_new_winner = document.getElementById('js_new_winner');
 
 
     }
@@ -28,6 +30,7 @@ class Root {
             data_output = JSON.parse(arr);
             self.render_data(data_output);
             self.render_winners(data_output);
+            self.add_new_winner(data_output);
         });
     }
 
@@ -62,20 +65,18 @@ class Root {
     }
 
     render_winners(data) {
-
         let self = this;
         let data_output = null;
-        let arr_winners_id = [];
         let winners_string = '';
         this.readTextFile(this.json_winners_path, function (arr) {
             data_output = JSON.parse(arr);
             for (let i = 0; i < data_output.length; i++) {
-                arr_winners_id.push(data_output[i]["id"]);
+                this.arr_winners_id.push(data_output[i]["id"]);
             }
             for (let index in data) {
-                for (let i = 0; i < arr_winners_id.length; i++) {
+                for (let i = 0; i < this.arr_winners_id.length; i++) {
 
-                    if (data[index]["id"] === arr_winners_id[i]) {
+                    if (data[index]["id"] === this.arr_winners_id[i]) {
                         winners_string += `${data[index]["name"]} ${data[index]["surname"]}   `
                     }
 
@@ -84,6 +85,41 @@ class Root {
             self.winners_output.value = winners_string;
         });
 
+
+    }
+
+    random_Number(min, max) {
+        let rand = min - 0.5 + Math.random() * (max - min + 1);
+        return Math.round(rand);
+    }
+
+    add_new_winner(data) {
+        let self = this;
+        let winner_id = 0;
+        let no_find_win = true;
+        this.btn_new_winner.addEventListener("click", function () {
+            while(no_find_win){
+               winner_id = this.random_Number(data[0]["id"], data[data.length - 1]["id"]);
+               no_find_win = self.arr_winners_id.includes(winner_id);
+            }
+
+        });
+
+    }
+
+    add_new_user() {
+
+    }
+
+    delete_new_user() {
+
+    }
+
+    edit_new_user() {
+
+    }
+
+    validation_form() {
 
     }
 
